ef3159b99053fecc9797baa283f27abb82a467d8
ANSozontov <A.N.Sozontov@gmail.com>
2023-11-11 20:10:49 +0500

    gitignore update

---
+++ monitor.R
@@ -0,0 +1,18 @@
+a <- system("sensors", intern = TRUE) |>
+    stringr::str_subset("temp") |>
+    stringr::str_replace_all("temp1:|[:space:]+|\\+|\\((.*?)\\)|°C", "") |>
+    as.numeric() |>
+    as.matrix() |>
+    t() |>
+    as.data.frame() |>
+    dplyr::mutate(tm = Sys.time())
+
+suppressWarnings(
+    write.table(
+        a,
+        "/home/orangepi/monitor/temperature.csv",
+        append = TRUE,
+        sep = ",",
+        row.names = FALSE,
+        col.names = !file.exists("/home/orangepi/monitor/temperature.csv"))
+)
